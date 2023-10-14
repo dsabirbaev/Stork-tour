0579d578d84c674113318ea710f163ab9635319b
Davranbek <davran9559@gmail.com>
2023-10-15 00:59:12 +0500

    fixed intro carousel

--- src/components/Intro/index.jsx
+++ src/components/Intro/index.jsx
@@ -2,6 +2,7 @@
 
 import { Message } from "../Icons";
 import { useTranslation } from "react-i18next";
+import VerticalCarousel from "../UI/VerticalCarousel"
 import "./style.scss";
 
 const index = () => {
@@ -9,10 +10,10 @@ const index = () => {
     return (
         <section id="intro" className="pt-[222px] md:pt-[324px] h-[600px] sm:h-[900px]">
             <div className="container">
-                <div className="flex flex-col gap-y-[20px] text-white">
-                    <h1 className="font-['MontserratSemiBold'] w-[550px] text-[36px] sm:text-[64px] leading-[50px] sm:leading-[89.6px]">{t("introTitle")}</h1>
-                    <p className="font-['NunitoRegular'] text-[16px] sm:text-[24px] w-[352px]">{t("introText")}</p>
-                </div>
+                
+               
+                <VerticalCarousel/>
+              
 
                 <div className="w-[60px] md:w-[100px] h-[60px] md:h-[100px] bg-[#0154A7] rounded-full flex items-center justify-center cursor-pointer fixed right-[3%] top-[80%] z-40">
                     <Message/>
---
+++ src/components/UI/VerticalCarousel/index.jsx
@@ -0,0 +1,73 @@
+
+
+import { useTranslation } from "react-i18next";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+import "./style.scss";
+const index = () => {
+    const { t } = useTranslation();
+    return (
+        <div>
+            <Swiper
+                // direction={'vertical'}
+                slidesPerView={1}
+               
+                pagination={{
+                    clickable: true,
+                }}
+                loop={true}
+                modules={[Pagination]}
+                className="h-[350px] sm:h-[500px] relative"
+
+                breakpoints={{
+                   
+                    500: {
+                      width: 500,
+                      direction:'vertical'
+                    }
+                }}
+            >
+                <SwiperSlide>
+                    <div className="flex flex-col gap-y-[20px] text-white">
+                        <h1 className="font-['MontserratSemiBold'] w-[550px] text-[36px] sm:text-[64px] leading-[50px] sm:leading-[89.6px]">{t("introTitle")}</h1>
+                        <p className="font-['NunitoRegular'] text-[16px] sm:text-[24px] w-[352px]">{t("introText")}</p>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="flex flex-col gap-y-[20px] text-white">
+                        <h1 className="font-['MontserratSemiBold'] w-[550px] text-[36px] sm:text-[64px] leading-[50px] sm:leading-[89.6px]">{t("introTitle")}</h1>
+                        <p className="font-['NunitoRegular'] text-[16px] sm:text-[24px] w-[352px]">{t("introText")}</p>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="flex flex-col gap-y-[20px] text-white">
+                        <h1 className="font-['MontserratSemiBold'] w-[550px] text-[36px] sm:text-[64px] leading-[50px] sm:leading-[89.6px]">{t("introTitle")}</h1>
+                        <p className="font-['NunitoRegular'] text-[16px] sm:text-[24px] w-[352px]">{t("introText")}</p>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="flex flex-col gap-y-[20px] text-white">
+                        <h1 className="font-['MontserratSemiBold'] w-[550px] text-[36px] sm:text-[64px] leading-[50px] sm:leading-[89.6px]">{t("introTitle")}</h1>
+                        <p className="font-['NunitoRegular'] text-[16px] sm:text-[24px] w-[352px]">{t("introText")}</p>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className="flex flex-col gap-y-[20px] text-white">
+                        <h1 className="font-['MontserratSemiBold'] w-[550px] text-[36px] sm:text-[64px] leading-[50px] sm:leading-[89.6px]">{t("introTitle")}</h1>
+                        <p className="font-['NunitoRegular'] text-[16px] sm:text-[24px] w-[352px]">{t("introText")}</p>
+                    </div>
+                </SwiperSlide>
+
+            </Swiper>
+        </div>
+    );
+}
+
+export default index;
